3ed26e91c05e93a6f40505a1a21a62bbb733762e
Simen Fritsvold <simefr@stud.ntnu.no>
2024-09-11 21:45:27 +0200

    test commit

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ const server = app.listen(port, () => {
 process.on("SIGTERM", () => {
   console.log("SIGTERM signal received: closing HTTP server");
   server.close(() => {
-    console.log("HTTP server closed");
+    console.log("HTTP server closed!");
   });
 });
